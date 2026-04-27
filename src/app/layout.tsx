306fc46b1be35d7b63dd0e8c2fe557183ae695fd
Kanop Sutharomna <78390700+arckanop@users.noreply.github.com>
2026-04-28 00:07:00 +0700

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,32 @@
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
+
+import {IBM_Plex_Sans_Thai as IBMPlexSansThai, JetBrains_Mono} from "next/font/google";
 import "./globals.css";
 import {Toaster} from "@/components/ui/sonner";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
+const ibmPlexSansThai = IBMPlexSansThai({
+	variable: "--font-ibm-plex-sans-thai",
+	subsets: ["thai", "latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700"]
+})
 
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
+const jetbrainsMono = JetBrains_Mono({
+	variable: "--font-jetbrains-mono",
 	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "Triam Udom Movie Booking",
-	description: "Seat booking portal for Triam Udom Suksa School movie events.",
+	title: "Triamudom Family Movie Booking",
+	description: "Registration for Triamudom Family Movie Booking.",
 };
 
 export default function RootLayout({
 	                                   children,
                                    }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}>
-		<body className="min-h-full flex flex-col bg-background text-foreground">
+		<html lang="en" className={`${ibmPlexSansThai.variable} ${jetbrainsMono.variable} h-full antialiased dark`}>
+		<body className="min-h-full flex flex-col bg-background text-foreground font-sans">
 		{children}
 		<Toaster/>
 		</body>
